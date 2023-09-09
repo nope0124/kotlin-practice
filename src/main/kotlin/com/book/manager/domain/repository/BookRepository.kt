7e9ef80852562eef319835fe13942fd0e2c774ac
nope0124 <kami-ryo0124@ezweb.ne.jp>
2023-09-10 02:35:18 +0900

    詳細画面の実装

---
+++ src/main/kotlin/com/book/manager/domain/repository/BookRepository.kt
@@ -0,0 +1,11 @@
+package com.book.manager.domain.repository
+
+import com.book.manager.domain.model.Book
+import com.book.manager.domain.model.BookWithRental
+import java.time.LocalDate
+
+interface BookRepository {
+    fun findAllWithRental(): List<BookWithRental>
+
+    fun findWithRental(id: Long): BookWithRental?
+}
